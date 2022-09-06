09004a08bf38e96ffb09933429f8b0f334238ea2
Mastereloading <guilherme-lamon@hotmail.com>
2022-09-05 22:35:40 -0300

    Vitejs typescript initial content

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Guilherme Mastereloading</h1>
+      <a href="https://github.com/Mastereloading"  target="blank">Github</a>
+    </div>
+  )
+}
+
+export default App
