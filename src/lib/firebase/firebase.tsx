575aecc01c03829a50cbc0f219f32ffbaaafc612
Mastereloading <guilherme-lamon@hotmail.com>
2022-09-15 19:04:50 -0300

    Configured vercel to use vite

--- src/lib/firebase/firebase.tsx
+++ src/lib/firebase/firebase.tsx
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, addDoc, collection } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_APIKEY,
-  authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECTID
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID
 }
 
 const firebase = initializeApp(firebaseConfig)
