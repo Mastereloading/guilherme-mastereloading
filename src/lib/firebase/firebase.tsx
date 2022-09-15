7b8946670df7e5c274cddf0101ea9682b223dbbc
Mastereloading <guilherme-lamon@hotmail.com>
2022-09-15 20:13:14 -0300

    Fixed vercel bugs

--- src/lib/firebase/firebase.tsx
+++ src/lib/firebase/firebase.tsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getFirestore, getDocs, addDoc, collection } from "firebase/firestore"
+import { getFirestore, addDoc, collection } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -7,19 +7,12 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID
 }
 
-console.log('VERCEL:   ' + import.meta.env.VERCEL)
-console.log('VERCEL_ENV:   ' + import.meta.env.VERCEL_ENV)
-console.log('DEV:   ' + import.meta.env.DEV)
-
 const firebase = initializeApp(firebaseConfig)
 const firestore = getFirestore(firebase)
 
 export const saveMessage = async ( message: string ) => {
   const collectionRef = collection(firestore, 'mastereloadingwebsitemessages')
   await addDoc(collectionRef, { message })
-
-  const test = await getDocs(collectionRef)
-  console.log(test.docs[0].data().message)
 
   return '200'
 }
