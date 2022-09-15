25c378362dccb1f0f6a788329b41997eb6d0cc77
Mastereloading <guilherme-lamon@hotmail.com>
2022-09-15 18:02:58 -0300

    Add next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ...
+  env: {
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+    FIREBASE_AUTHDOMAIN: process.env.FIREBASE_AUTHDOMAIN,
+    FIREBASE_PROJECTID: process.env.FIREBASE_PROJECTID
+  }
+}
