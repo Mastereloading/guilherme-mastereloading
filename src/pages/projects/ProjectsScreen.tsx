e3b4632fdd289536a569ef40ed72bdd25f36c6e9
Mastereloading <guilherme-lamon@hotmail.com>
2023-08-11 23:11:42 -0300

    Added new project

--- src/pages/projects/ProjectsScreen.tsx
+++ src/pages/projects/ProjectsScreen.tsx
@@ -2,8 +2,8 @@ import WorkCard from "../../components/workcard/WorkCard"
 import { Container } from "./styles"
 
 import project1 from '../../assets/workcardimage/project1.png'
-import project2 from '../../assets/workcardimage/project2.png'
 import project3 from '../../assets/workcardimage/project3.png'
+import blank from '../../assets/workcardimage/blank.png'
 
 const Projects = () => {
   const cardsData = [{
@@ -12,16 +12,22 @@ const Projects = () => {
       description: 'Este próprio site já é um dos meus projetos, me desafiei a criar um portfólio em 1 semana e o resultado você está acompanhando agora. (Continuarei editando ele, pretendo implementar algum banco de dados e adicionar algumas APIs...)',
       linkto: 'https://github.com/Mastereloading/guilherme-mastereloading'
     }, {
-      image: project2,
+      image: blank,
+      title: 'AI Story',
+      description: 'Integrado a inteligência artificial este site cria uma imagem e uma historia para um personagem fictício. Site desenvolvido para estudar Next/Tailwind e APIs de inteligências artificiais.',
+      linkto: 'https://github.com/Mastereloading/ai-story'
+    }, {
+      image: project3,
       title: 'Coup - Mobile Card Game',
       description: 'Amo jogar este jogo de cartas, porém não existe nenhuma versão dele para celular, então resolvi criar uma própria, assim aperfeiçoo meus trabalhos de design e ainda ganho um jogo novo. (Projeto em desenvolvimento)',
       linkto: 'https://github.com/Mastereloading/coup-app'
-    }, {
-      image: project3,
-      title: 'EM BREVE',
-      description: 'Qualquer outra atualização ou projeto novo será postado aqui...',
-      linkto: ''
     }
+    // , {
+    //   image: project3,
+    //   title: 'EM BREVE',
+    //   description: 'Qualquer outra atualização ou projeto novo será postado aqui...',
+    //   linkto: ''
+    // }
   ]
 
   return (
